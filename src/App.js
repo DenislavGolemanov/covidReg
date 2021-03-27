3e95997eada7ac6e203b462fc2f8e02ca838f85b
DenislavGolemanov <denislav.golemanov@gmail.com>
2021-03-27 19:29:10 +0200

    Routing Initiated
    Home view created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import {Route , Switch} from 'react-router-dom'
+
 import Header from '../src/components/Header/Header'
 import Counter from '../src/components/Counter/Counter'
 import Footer from '../src/components/Footer/Footer'
 import Info from '../src/components/Info/Info'
+import HomeView from '../src/components/HomePage/HomePage'
 
 
 function App() {
@@ -13,9 +16,16 @@ function App() {
 
       <Header/>
 
-      <Counter />
+      <Switch>
+          <Route path="/" exact component={HomeView} />
+          {/* <Route path="/information" component="HomeView" />
+          <Route path="/registration" component="HomeView" /> */}
+      </Switch>
 
-      <Info />
+
+      {/* <Counter />
+
+      <Info /> */}
 
       <Footer />
 
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import Counter from '../Counter/Counter'
+import Info from '../Info/Info'
+
+
+function HomeView(props) {
+
+    return (
+        <div>
+
+            <Counter />
+            <Info />
+
+        </div>
+        
+    )
+}
+
+export default HomeView
